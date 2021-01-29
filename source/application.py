77b3b3a28b63c6e4d66bbcf5dd079bd033ac4688
Alexander Demin <alexander@demin.ws>
2021-01-29 10:26:31 +0000

    Add instructions catalog

--- source/application.py
+++ source/application.py
@@ -3,9 +3,11 @@ from flask import (
     render_template,
 )
 
+from opcodes import opcodes
+
 application = Flask(__name__)
 
 
 @application.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', opcodes=opcodes)
